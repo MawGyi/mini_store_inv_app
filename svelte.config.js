3a227563f8a32989a497b15c5cd209ea8073727c
MawGyi <winmawoo89@gmail.com>
2026-01-06 11:16:00 +0700

    config: add Vercel deployment configuration
    - Install @sveltejs/adapter-vercel
    - Configure svelte.config.js for Vercel
    - Add vercel.json with security headers and caching
    - Add .env.example for environment variables

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      runtime: 'nodejs22.x',
+      regions: ['iad1'],
+      split: true
+    }),
+    alias: {
+      $lib: './src/lib',
+      $components: './src/lib/components'
+    }
   }
 }
